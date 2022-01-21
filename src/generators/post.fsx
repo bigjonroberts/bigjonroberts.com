a5d4fd6b42bd7777fc2be683661dedaed2b483e9
Jon Roberts <bigjonroberts@users.noreply.github.com>
2022-01-21 16:42:25 -0600

    remove debug output

--- src/generators/post.fsx
+++ src/generators/post.fsx
@@ -6,45 +6,32 @@ open Html
 
 let generate' (ctx : SiteContents) (page: string) =
     let post =
-        match ctx.TryGetValues<Postloader.Post> () with
-        | None ->
-            let key = typeof<List<Postloader.Post>>
-            key |> printfn "Service not found for key '%A'"
-            None
-        | Some xs ->
-            xs |> Seq.tryFind (fun n ->
-                printfn "checking if '%s' matches '%s'" n.file page
-                n.file = page)
+        ctx.TryGetValues<Postloader.Post> ()
+        |> Option.defaultValue Seq.empty
+        |> Seq.find (fun n -> n.file = page)
 
-    match post with
-    | Some post ->
-        let siteInfo = ctx.TryGetValue<Globalloader.SiteInfo> ()
-        let desc =
-            siteInfo
-            |> Option.map (fun si -> si.description)
-            |> Option.defaultValue ""
+    let siteInfo = ctx.TryGetValue<Globalloader.SiteInfo> ()
+    let desc =
+        siteInfo
+        |> Option.map (fun si -> si.description)
+        |> Option.defaultValue ""
 
-        Layout.layout ctx post.title [
-            section [Class "hero is-info is-medium is-bold"] [
-                div [Class "hero-body"] [
-                    div [Class "container has-text-centered"] [
-                        h1 [Class "title"] [!!desc]
-                    ]
-                ]
-            ]
-            div [Class "container"] [
-                section [Class "articles"] [
-                    div [Class "column is-8 is-offset-2"] [
-                        Layout.postLayout false post
-                    ]
+    Layout.layout ctx post.title [
+        section [Class "hero is-info is-medium is-bold"] [
+            div [Class "hero-body"] [
+                div [Class "container has-text-centered"] [
+                    h1 [Class "title"] [!!desc]
                 ]
             ]
         ]
-    | None ->
-        printfn "page '%s' not found" page
-        html [] [ !! "page not found" ]
-
-
+        div [Class "container"] [
+            section [Class "articles"] [
+                div [Class "column is-8 is-offset-2"] [
+                    Layout.postLayout false post
+                ]
+            ]
+        ]
+    ]
 
 let generate (ctx : SiteContents) (projectRoot: string) (page: string) =
     generate' ctx page
