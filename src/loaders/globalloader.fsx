3b3cf8a4a64f77f45da54ce795f5a95e35f90da0
Jon Roberts <bigjonroberts@users.noreply.github.com>
2022-01-21 09:09:41 -0600

    Change site title and description

--- src/loaders/globalloader.fsx
+++ src/loaders/globalloader.fsx
@@ -8,8 +8,8 @@ type SiteInfo = {
 
 let loader (projectRoot: string) (siteContent: SiteContents) =
     let siteInfo =
-        { title = "Sample Fornax blog";
-          description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        { title = "Big Jon Roberts";
+          description = "The man, the myth"
           postPageSize = 5 }
     siteContent.Add(siteInfo)
 
